8467d1523edc4b01e019f713f993e30442a32537
Jeremiah Lowe <jeremiahbl@protonmail.com>
2022-12-03 20:16:37 -0500

    Permission support and configuration support

--- src/main/java/com/jeremiahbl/bfcmod/events/ChatEventHandler.java
+++ src/main/java/com/jeremiahbl/bfcmod/events/ChatEventHandler.java
@@ -3,29 +3,44 @@ package com.jeremiahbl.bfcmod.events;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import com.jeremiahbl.bfcmod.BetterForgeChat;
-import com.jeremiahbl.bfcmod.BetterForgeChatUtilities;
 import com.jeremiahbl.bfcmod.TextFormatter;
+import com.jeremiahbl.bfcmod.config.ConfigHandler;
+import com.jeremiahbl.bfcmod.config.PermissionsHandler;
+import com.jeremiahbl.bfcmod.utils.BetterForgeChatUtilities;
 
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.event.ServerChatEvent;
+import net.minecraftforge.event.server.ServerStartedEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.server.permission.*;
+import net.minecraftforge.server.permission.nodes.*;
 
 @EventBusSubscriber
 public class ChatEventHandler {
+	private SimpleDateFormat timestampFormat = null;
+	
+	@SubscribeEvent
+	public void onServerLoad(ServerStartedEvent e) {
+		timestampFormat = ConfigHandler.config.enableTimestamp.get().booleanValue() ? new SimpleDateFormat(ConfigHandler.config.timestampFormat.get()) : null;
+	}
 	@SubscribeEvent
     public void onServerChat(ServerChatEvent e) {
-    	if(e == null) return;
-    	String tstamp = new SimpleDateFormat("hh:mm").format(new Date());
-		String user = e.getUsername();
+    	ServerPlayer player = e.getPlayer();
+    	if(e == null || player == null) return;
 		String msg = e.getMessage();
-		if(user == null || msg == null) return;
-		if(msg.length() <= 0) return;
-		
-		if(BetterForgeChat.instance.perms != null)
-			user = BetterForgeChatUtilities.getRawPlayerName(e.getPlayer());
-		
-		msg = String.format("%s | %s: %s", tstamp, user, msg);
-		e.setComponent(TextFormatter.stringToFormattedText(msg));
+		if(msg == null || msg.length() <= 0) return;
+    	String tstamp = timestampFormat == null ? "" : timestampFormat.format(new Date());
+		String name = BetterForgeChatUtilities.getRawPreferredPlayerName(player);
+		String fmat = ConfigHandler.config.chatMessageFormat.get().replace("$time", tstamp).replace("$name", name);
+		TextComponent beforeMsg = TextFormatter.stringToFormattedText(fmat.substring(0, fmat.indexOf("$msg")));
+		TextComponent afterMsg = TextFormatter.stringToFormattedText(fmat.substring(fmat.indexOf("$msg") + 4, fmat.length()));
+		Boolean enableColor = PermissionAPI.getPermission(player, PermissionsHandler.coloredChatNode, new PermissionDynamicContext[0]);
+		Boolean enableStyle = PermissionAPI.getPermission(player, PermissionsHandler.styledChatNode, new PermissionDynamicContext[0]);
+		if(enableColor == null) enableColor = true;
+		if(enableStyle == null) enableStyle = true;
+		TextComponent msgComp = TextFormatter.stringToFormattedText(msg, enableColor, enableStyle);
+		e.setComponent(beforeMsg.append(msgComp.append(afterMsg)));
     }
 }
