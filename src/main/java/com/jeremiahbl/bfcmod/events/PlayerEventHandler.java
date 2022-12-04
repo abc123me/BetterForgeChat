48bc970bc04d6667aa0ce7c2b06d08e170ffe328
Jeremiah Lowe <jeremiahbl@protonmail.com>
2022-12-04 02:49:20 -0500

    Fixed tab list out-of-date bug

--- src/main/java/com/jeremiahbl/bfcmod/events/PlayerEventHandler.java
+++ src/main/java/com/jeremiahbl/bfcmod/events/PlayerEventHandler.java
@@ -1,10 +1,12 @@
 package com.jeremiahbl.bfcmod.events;
 
 import com.jeremiahbl.bfcmod.config.ConfigHandler;
+import com.jeremiahbl.bfcmod.config.PermissionsHandler;
 import com.jeremiahbl.bfcmod.utils.BetterForgeChatUtilities;
 
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.entity.player.PermissionsChangedEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent.NameFormat;
 import net.minecraftforge.event.entity.player.PlayerEvent.TabListNameFormat;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -16,10 +18,14 @@ public class PlayerEventHandler {
 	public void onTabListNameFormatEvent(TabListNameFormat e) {
 		if(ConfigHandler.config.enableTabListIntegration.get()) {
 			Player player = e.getPlayer();
-			if(player instanceof ServerPlayer)
-				e.setDisplayName(BetterForgeChatUtilities.getFormattedPlayerName((ServerPlayer) player,
-						ConfigHandler.config.enableNicknamesInTabList.get(),
-						ConfigHandler.config.enableMetadataInTabList.get()));
+			if(player instanceof ServerPlayer) {
+				ServerPlayer sexyPlayer = (ServerPlayer) player;
+				boolean allowNickname = ConfigHandler.config.enableNicknamesInTabList.get();
+				boolean allowMetadata = ConfigHandler.config.enableMetadataInTabList.get();
+				if(allowNickname) allowNickname = PermissionsHandler.playerHasPermission(sexyPlayer, PermissionsHandler.tabListNicknameNode);
+				if(allowMetadata) allowMetadata = PermissionsHandler.playerHasPermission(sexyPlayer, PermissionsHandler.tabListMetadataNode);
+				e.setDisplayName(BetterForgeChatUtilities.getFormattedPlayerName(sexyPlayer, allowNickname, allowMetadata));
+			}
 		}
 	}
 	@SubscribeEvent
@@ -27,5 +33,15 @@ public class PlayerEventHandler {
 		Player player = e.getPlayer();
 		if(player instanceof ServerPlayer) 
 			e.setDisplayname(BetterForgeChatUtilities.getFormattedPlayerName((ServerPlayer) player));
+	}
+	@SubscribeEvent
+	public void onPermissionsChanged(PermissionsChangedEvent e) {
+		Player player = e.getPlayer();
+		if(player instanceof ServerPlayer) {
+			// Update tab list name so if the players group changes or there tab
+			// list permissions are revoked the tab list name gets updated
+			ServerPlayer sexyPlayer = (ServerPlayer) player;
+			sexyPlayer.refreshTabListName();
+		}
 	}
 }
