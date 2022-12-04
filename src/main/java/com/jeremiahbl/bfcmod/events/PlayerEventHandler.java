0a2dac83103daaf859815b6d6abb1c96af56779a
Jeremiah Lowe <jeremiahbl@protonmail.com>
2022-12-03 20:16:11 -0500

    Removed unecessary check

--- src/main/java/com/jeremiahbl/bfcmod/events/PlayerEventHandler.java
+++ src/main/java/com/jeremiahbl/bfcmod/events/PlayerEventHandler.java
@@ -1,7 +1,6 @@
 package com.jeremiahbl.bfcmod.events;
 
-import com.jeremiahbl.bfcmod.BetterForgeChat;
-import com.jeremiahbl.bfcmod.BetterForgeChatUtilities;
+import com.jeremiahbl.bfcmod.utils.BetterForgeChatUtilities;
 
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
@@ -14,18 +13,14 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 public class PlayerEventHandler {
 	@SubscribeEvent
 	public void onTabListNameFormatEvent(TabListNameFormat e) {
-		if(BetterForgeChat.instance.perms != null) {
-			Player player = e.getPlayer();
-			if(player instanceof ServerPlayer)
-				e.setDisplayName(BetterForgeChatUtilities.getFormattedPlayerName((ServerPlayer) player));
-		}
+		Player player = e.getPlayer();
+		if(player instanceof ServerPlayer)
+			e.setDisplayName(BetterForgeChatUtilities.getFormattedPlayerName((ServerPlayer) player));
 	}
 	@SubscribeEvent
 	public void onNameFormatEvent(NameFormat e) {
-		if(BetterForgeChat.instance.perms != null) {
-			Player player = e.getPlayer();
-			if(player instanceof ServerPlayer) 
-				e.setDisplayname(BetterForgeChatUtilities.getFormattedPlayerName((ServerPlayer) player));
-		}
+		Player player = e.getPlayer();
+		if(player instanceof ServerPlayer) 
+			e.setDisplayname(BetterForgeChatUtilities.getFormattedPlayerName((ServerPlayer) player));
 	}
 }
